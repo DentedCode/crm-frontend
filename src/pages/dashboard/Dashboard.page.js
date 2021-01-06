8674e752f8218131abd459f664a565932ab1519e
Prem Acharya <prem.acharya@hotmail.com>
2021-01-06 17:47:33 +1100

    dashboard data fetched and updated

--- src/pages/dashboard/Dashboard.page.js
+++ src/pages/dashboard/Dashboard.page.js
@@ -1,12 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import { Container, Row, Col, Button } from "react-bootstrap";
 import { TicketTable } from "../../components/ticket-table/TicketTable.comp";
 import tickets from "../../assets/data/dummy-tickets.json";
 import { PageBreadcrumb } from "../../components/breadcrumb/Breadcrumb.comp";
-
 import { Link } from "react-router-dom";
 
+import { fetchAllTickets } from "../ticket-list/ticketsAction";
+
 export const Dashboard = () => {
+  const dispatch = useDispatch();
+  const { tickets } = useSelector((state) => state.tickets);
+
+  useEffect(() => {
+    if (!tickets.length) {
+      dispatch(fetchAllTickets());
+    }
+  }, [tickets, dispatch]);
+
+  const pendingTickets = tickets.filter((row) => row.status !== "Closed");
+  const totlatTickets = tickets.length;
   return (
     <Container>
       <Row>
@@ -28,8 +41,8 @@ export const Dashboard = () => {
       </Row>
       <Row>
         <Col className="text-center  mb-2">
-          <div>Total tickets: 50</div>
-          <div>Pending tickets: 5</div>
+          <div>Total tickets: {totlatTickets}</div>
+          <div>Pending tickets: {pendingTickets.length}</div>
         </Col>
       </Row>
       <Row>

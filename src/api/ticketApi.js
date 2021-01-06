61b09deec825db8ecb4ef0ca693e177e2ae98a41
Prem Acharya <prem.acharya@hotmail.com>
2021-01-06 11:35:00 +1100

    fetch all tickets api integrated

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -5,8 +5,7 @@ export const getAllTickets = () => {
     try {
       const result = await axios.get("http://localhost:3001/v1/ticket", {
         headers: {
-          Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImUyQGUuY29tIiwiaWF0IjoxNjA4MTU5MzE0LCJleHAiOjE2MDgyNDU3MTR9.59n2aDsOvpKQOk7JG8lcIXVXFNxk4MWdEfZsYBG60FE",
+          Authorization: sessionStorage.getItem("accessJWT"),
         },
       });
 
